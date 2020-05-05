9de900c0ecd3e4ba593de2542855ec44d54ec38d
angel.broncanorodrig <angel.broncanorodriguez@plexus.es>
2020-05-05 10:30:44 +0200

    #f/149591-Secciones
    Se incorpora el esquema a todas los sql

--- src/main/resources/db/changelog/sql/data-test/010-configuration_sections.sql
+++ src/main/resources/db/changelog/sql/data-test/010-configuration_sections.sql
@@ -1,18 +1,18 @@
 ----------------------------------------------------- CONFIGURACIÓN ----------------------------------------------------
 -- Section: Sections
-INSERT INTO sections(
+INSERT INTO hopes.sections(
 	sec_id, sec_title, sec_description, sec_active, sec_order, sec_section_root, sec_icon, sec_url)
-VALUES (nextval('sections_sec_id_seq'),
+VALUES (nextval('hopes.sections_sec_id_seq'),
         'SECTIONS',
         'Secciones',
         true,
-        (select max (sec_order)+1 from sections where sec_section_root = (select sec_id from sections where sec_title ='HOPES' LIMIT 1)),
-        (select sec_id from sections where sec_title ='HOPES' LIMIT 1),
+        (select max (sec_order)+1 from hopes.sections where sec_section_root = (select sec_id from hopes.sections where sec_title ='HOPES' LIMIT 1)),
+        (select sec_id from hopes.sections where sec_title ='HOPES' LIMIT 1),
         'assets/img/modules/secciones.png',
         '#');
 
 -- Role_Admin
-INSERT INTO sections_roles(scr_id, scr_section_id, scr_role_id)
-VALUES (nextval('sections_roles_scr_id_seq'),
-		(select sec_id from sections where sec_title ='SECTIONS' LIMIT 1),
-		(select rol_id from roles where rol_name = 'ROLE_ADMIN' LIMIT 1));
+INSERT INTO hopes.sections_roles(scr_id, scr_section_id, scr_role_id)
+VALUES (nextval('hopes.sections_roles_scr_id_seq'),
+		(select sec_id from hopes.sections where sec_title ='SECTIONS' LIMIT 1),
+		(select rol_id from hopes.roles where rol_name = 'ROLE_ADMIN' LIMIT 1));
--- src/main/resources/db/changelog/sql/schema/006-schema.sql
+++ src/main/resources/db/changelog/sql/schema/006-schema.sql
@@ -1,2 +1,2 @@
-ALTER TABLE forms ADD for_template varchar(100) NULL;
-COMMENT ON COLUMN forms.for_template IS 'Columna que relaciona el formulario con una plantilla de la bbdd no relacional';
+ALTER TABLE hopes.forms ADD for_template varchar(100) NULL;
+COMMENT ON COLUMN hopes.forms.for_template IS 'Columna que relaciona el formulario con una plantilla de la bbdd no relacional';
