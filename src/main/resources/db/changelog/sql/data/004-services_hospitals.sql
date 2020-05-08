05cb6d577fa1aa1dfd5d817730c8ae48b63a279b
angel.broncanorodrig <angel.broncanorodriguez@plexus.es>
2020-05-08 09:19:59 +0200

    #r/v0.1.0
    Se suben los scripts

---
+++ src/main/resources/db/changelog/sql/data/004-services_hospitals.sql
@@ -0,0 +1,33 @@
+WITH serviceOne AS (
+         INSERT INTO hopes.services (srv_id, srv_name, srv_description) VALUES (nextval('hopes.services_srv_id_seq'), 'Dermatología', 'Servicio de dermatologia')
+             RETURNING srv_id
+     ),
+     serviceTwo AS (
+         INSERT INTO hopes.services (srv_id, srv_name, srv_description) VALUES (nextval('hopes.services_srv_id_seq'), 'Reumatología', 'Servicio de Reumatología')
+             RETURNING srv_id
+     ),
+     serviceThree AS (
+         INSERT INTO hopes.services (srv_id, srv_name, srv_description) VALUES (nextval('hopes.services_srv_id_seq'), 'Cardiología', 'Servicio de cardiología')
+             RETURNING srv_id
+     ),
+     serviceFour AS (
+         INSERT INTO hopes.services (srv_id, srv_name, srv_description) VALUES (nextval('hopes.services_srv_id_seq'), 'Cuidados paliativos', 'Servicio de Cuidados paliativos')
+             RETURNING srv_id
+     ),
+     hospital_services_one AS (
+         INSERT INTO hopes.hospitals_services (hss_id, hss_hos_id, hss_srv_id)
+             VALUES (nextval('hopes.hospitals_services_hss_id_seq'), (SELECT hos_id FROM hopes.hospitals WHERE hos_name = 'Hopes - Servicios de Salud'),
+                     (SELECT srv_id FROM serviceOne))
+     ),
+     hospital_services_two AS (
+         INSERT INTO hopes.hospitals_services (hss_id, hss_hos_id, hss_srv_id)
+             VALUES (nextval('hopes.hospitals_services_hss_id_seq'), (SELECT hos_id FROM hopes.hospitals WHERE hos_name = 'Hopes - Servicios de Salud'),
+                     (SELECT srv_id FROM serviceTwo))
+     ),
+     hospital_services_three AS (
+         INSERT INTO hopes.hospitals_services (hss_id, hss_hos_id, hss_srv_id)
+             VALUES (nextval('hopes.hospitals_services_hss_id_seq'), (SELECT hos_id FROM hopes.hospitals WHERE hos_name = 'Hopes - Servicios de Salud'),
+                     (SELECT srv_id FROM serviceOne))
+     ) INSERT INTO hopes.hospitals_services (hss_id, hss_hos_id, hss_srv_id)
+     VALUES (nextval('hopes.hospitals_services_hss_id_seq'), (SELECT hos_id FROM hopes.hospitals WHERE hos_name = 'Hopes - Servicios de Salud'),
+                     (SELECT srv_id FROM serviceThree));
