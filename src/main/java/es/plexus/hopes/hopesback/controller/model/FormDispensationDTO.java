24eaa16b0b42005b2607ace2d076f21c77ec002e
juan.fernandezgranad <juan.fernandezgranados@plexus.es>
2020-05-11 10:33:14 +0200

    Cambio Formato fechas

--- src/main/java/es/plexus/hopes/hopesback/controller/model/FormDispensationDTO.java
+++ src/main/java/es/plexus/hopes/hopesback/controller/model/FormDispensationDTO.java
@@ -1,26 +1,29 @@
 package es.plexus.hopes.hopesback.controller.model;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.NotBlank;
+
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotBlank;
-import java.time.LocalDateTime;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 @Data
 @ApiModel
 public class FormDispensationDTO {
 
-	@ApiModelProperty(position = 10, example = "1981-01-01T00:00:00Z", value = "Inicio del período de las dispensaciones")
-	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	
+	@ApiModelProperty(position = 10, example = "01-01-1981 00:00:00", value = "Inicio del período de las dispensaciones")
+	@DateTimeFormat(pattern = "dd-MM-yyyy HH:mm:ss")
 	@NotBlank
 	private LocalDateTime startPeriod;
 
-	@ApiModelProperty(position = 20, example = "1981-01-01T00:00:00Z", value = "Fin del período de las dispensaciones")
+	@ApiModelProperty(position = 20, example = "01-01-1981 00:00:00", value = "Fin del período de las dispensaciones")
 	@NotBlank
-	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	@DateTimeFormat(pattern = "dd-MM-yyyy HH:mm:ss")
 	private LocalDateTime endPeriod;
 
 	@ApiModelProperty(position = 30, example = "1", value = "Fichero de carga con las dispensaciones")
