5e506c3e95c23f2028bee6da09481b2cb3128416
juan.fernandezgranad <juan.fernandezgranados@plexus.es>
2020-05-12 07:51:17 +0200

    f/150239-internacionalizacion-Data-Back

--- src/main/resources/db/changelog/sql/data-test/011-configuration_sections_locale.sql
+++ src/main/resources/db/changelog/sql/data-test/011-configuration_sections_locale.sql
@@ -1,18 +1,18 @@
 ----------------------------------------------------------------- LOCALIZED SECTIONS -------------------------------------------------------------------------------
-INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Hopes', 'Root', 1);
-INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Hopes', 'Raíz', 1);
+INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Hopes', 'Home', 1);
+INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Hopes', 'Home', 1);
 
-INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Calendar', 'System Calendar', 2);
-INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Calendario', 'Calendario del sitema', 2);
+INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Calendar', 'Section containing the Pathology Appointment Calendar', 2);
+INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Calendario', 'Sección que contiene el Calendario de citas de la patología', 2);
 
-INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Patients', 'Patients Administration', 3);
-INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Pacientes', 'Administración de Pacientes', 3);
+INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Patient', 'Section containing the complete Menu after selecting a Patient', 3);
+INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Paciente', 'Sección que contiene el Menú completo tras la seleccion de un Paciente', 3);
 
-INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Control Panel', 'Control Panel', 4);
-INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Panel de control', 'Panel de control del sitema', 4);
+INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Control Panel', 'Section containing the Pathology Scorecard', 4);
+INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Cuadro de Mando', 'Sección que contiene el Cuadro de Mando de la patología', 4);
 
-INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Alerts', 'Alerts System', 5);
-INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Alertas', 'Sitema de alertas', 5);
+INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Alerts', 'Section containing Pathology Alerts', 5);
+INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Alertas', 'Sección que contiene las Alertas de la patología', 5);
 
 INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Form Derma', 'Form derma', 6);
 INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Formulario Dermatología', 'Formulario Dermatología', 6);
@@ -20,11 +20,11 @@ INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_descrip
 INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Form VIH', 'Form Vih', 8);
 INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Formulario VIH', 'Formulario Vih', 8);
 
-INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Dispensations', 'Dispensations', 9);
-INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Dispensaciones', 'Dispensaciones', 9);
+INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Dispensations', 'Section containing the dispensation charge', 9);
+INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Dispensaciones', 'Sección que contiene la carga de dispensaciones', 9);
 
-INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Medicines', 'Medicines', 10);
-INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Medicamentos', 'Medicamentos', 10);
+INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Medicines Manager', 'Section containing Medicines Management', 10);
+INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Gestión de Medicamentos', 'Sección que contiene la Gestión de Medicamentos', 10);
 
 INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'en', 'Sections', 'Sections', 11);
 INSERT INTO hopes.localized_sections (lcs_id, lcs_locale, lcs_title, lcs_description, lcs_sec_id) VALUES (nextval('hopes.localized_sections_lcs_id_seq'), 'es', 'Secciones', 'Secciones', 11);
