f8f52efde82a76b6c048cb5315724bb31cb8cf93
Jose Estevez <joseeb@ext.inditex.com>
2020-06-10 10:01:25 +0200

    Process to migrate diagnosis data from non relational ddbb to relational ddbb

--- src/main/java/es/plexus/hopes/hopesback/service/mapper/PatientMapper.java
+++ src/main/java/es/plexus/hopes/hopesback/service/mapper/PatientMapper.java
@@ -5,7 +5,7 @@ import es.plexus.hopes.hopesback.repository.model.Patient;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface PatientMapper {
 
     PatientMapper INSTANCE = Mappers.getMapper(PatientMapper.class);
