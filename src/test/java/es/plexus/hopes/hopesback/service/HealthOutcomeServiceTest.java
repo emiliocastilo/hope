3828f84ca71949c71355ff687c0313eb76362b0c
angel.broncanorodrig <angel.broncanorodriguez@plexus.es>
2020-06-11 19:58:15 +0200

    #release/v1.0.0
    Se añaden un nuevo parámetro al servicio de HealthOutcomeService para traer los pacientes según resultado PASI

--- src/test/java/es/plexus/hopes/hopesback/service/HealthOutcomeServiceTest.java
+++ src/test/java/es/plexus/hopes/hopesback/service/HealthOutcomeServiceTest.java
@@ -1,7 +1,9 @@
-package es.plexus.hopes.hopesback.controller;
+package es.plexus.hopes.hopesback.service;
 
 import es.plexus.hopes.hopesback.controller.model.GraphPatientDetailDTO;
-import es.plexus.hopes.hopesback.service.HealthOutcomeService;
+import es.plexus.hopes.hopesback.repository.HealthOutcomeRepository;
+import es.plexus.hopes.hopesback.repository.model.HealthOutcome;
+import es.plexus.hopes.hopesback.repository.model.Patient;
 import org.hibernate.service.spi.ServiceException;
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,7 +20,6 @@ import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -27,104 +28,101 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
-public class HealthOutcomeControllerTest {
+public class HealthOutcomeServiceTest {
 
 	@Mock
+	private HealthOutcomeRepository healthOutcomeRepository;
+	
+	@InjectMocks
 	private HealthOutcomeService healthOutcomeService;
 
-	@InjectMocks
-	private HealthOutcomeController healthOutcomeController;
 
 
 	@Test
 	public void callFindResultsByTypesShouldBeStatusOk() {
 
 		// given
-		given(healthOutcomeService.findResultsByTypes(anyString()))
-				.willReturn(mockMapStringLong());
+		given(healthOutcomeRepository.findResultsByTypes(anyString()))
+				.willReturn(Collections.singletonList(mockHealthOutcome()));
 
 		// when
-		Map<String, Long> response = healthOutcomeController.findResultsByTypes(anyString());
+		Map<String, Long> response = healthOutcomeService.findResultsByTypes(anyString());
 
 		// then		
 		Assert.assertNotNull(response);
 	}
 		
-	@Test(expected = ServiceException.class)
-	public void callFindResultsByTypesThrowException() throws ServiceException {
-		// given
-		given(healthOutcomeService.findResultsByTypes(anyString()))
-				.willThrow(new ServiceException("Error: No contled error"));
 
-		// when
-		Map<String, Long> response = healthOutcomeController.findResultsByTypes(anyString());
-
-		Assert.assertEquals(HttpStatus.BAD_REQUEST, response);
-		Assert.assertNull(response);
-	}
-	
 	@Test
 	public void callDetailsResultsShouldBeStatusOk() {
 
 		// given
 		final PageRequest pageRequest = PageRequest.of(1, 5, Sort.by("patient"));
-		given(healthOutcomeService.getDetailsResultsByType(anyString(), anyString(), any(Pageable.class)))
+		given(healthOutcomeRepository.getDetailsResultsByType(anyString(), anyString(), any(Pageable.class)))
 				.willReturn(getPageableGraphPatientDetail(pageRequest));
 
 		// when
-		Page<GraphPatientDetailDTO> response = healthOutcomeController.getDetailsResultsByType("PASI", "PASI", pageRequest);
+		Page<GraphPatientDetailDTO> response = healthOutcomeService.getDetailsResultsByType("PASI", "PASI", pageRequest);
 
 		// then		
 		Assert.assertNotNull(response);
-		Assert.assertTrue(!response.isEmpty());
+		Assert.assertFalse(response.isEmpty());
 	}
 	
 	@Test(expected = ServiceException.class)
 	public void callDetailsResultsThrowException() throws ServiceException {
 		// given
 		final PageRequest pageRequest = PageRequest.of(1, 5, Sort.by("patient"));
-		given(healthOutcomeService.getDetailsResultsByType(anyString(),  anyString(), any(Pageable.class)))
+		given(healthOutcomeRepository.getDetailsResultsByType(anyString(),  anyString(), any(Pageable.class)))
 				.willThrow(new ServiceException("Error: No contled error"));
 
 		// when
-		Page<GraphPatientDetailDTO> response = healthOutcomeController.getDetailsResultsByType("PASI", "", pageRequest);
+		Page<GraphPatientDetailDTO> response = healthOutcomeService.getDetailsResultsByType("PASI", "", pageRequest);
 
 		Assert.assertEquals(HttpStatus.BAD_REQUEST, response);
 		Assert.assertNull(response);
 	}
 
-	public void callDetailsResultsExportShouldBeStatusOk() {
-		// given
-		given(healthOutcomeService.getDetailsResultsByType(anyString(), anyString()))
-				.willReturn(Collections.singletonList(mockGraphPatientDetailsDTO()));
-
-		// when
-		List<GraphPatientDetailDTO> response = healthOutcomeController.getDetailsResultsByType(anyString(), anyString());
-
-		Assert.assertEquals(HttpStatus.OK,response);
-		Assert.assertNull(response);
-	}
 
 	@Test(expected = ServiceException.class)
 	public void callDetailsResultsExportThrowException() throws ServiceException {
 		// given
-		given(healthOutcomeService.getDetailsResultsByType(anyString(),  anyString()))
+		given(healthOutcomeRepository.getDetailsResultsByType(anyString(),  anyString()))
 				.willThrow(new ServiceException("Error: No contled error"));
 
 		// when
-		List<GraphPatientDetailDTO> response = healthOutcomeController.getDetailsResultsByType("PASI", "");
+		List<GraphPatientDetailDTO> response = healthOutcomeService.getDetailsResultsByType("PASI", "");
 
 		Assert.assertEquals(HttpStatus.BAD_REQUEST, response);
 		Assert.assertNull(response);
 	}
-	
-	//Mocks
-	private Map<String, Long> mockMapStringLong() {
-		Map<String, Long> map = new HashMap<>();
-		map.put("Type", 3L);
-		return map;
+
+	@Test
+	public void callGetAllPatientsIdShouldBeStatusOk() {
+
+		// given
+		given(healthOutcomeRepository.getAllPatientsId()).willReturn(Collections.singletonList(1L));
+
+		// when
+		List<Long> response = healthOutcomeService.getAllPatientsId();
+
+		// then
+		Assert.assertNotNull(response);
+		Assert.assertFalse(response.isEmpty());
 	}
-	
+
+	//Mocks
+	private HealthOutcome mockHealthOutcome(){
+		HealthOutcome healthOutcome = new HealthOutcome();
+		healthOutcome.setPatient(new Patient());
+		healthOutcome.setDate(LocalDateTime.now());
+		healthOutcome.setId(1L);
+		healthOutcome.setIndexType("PASI");
+		healthOutcome.setResult("Result");
+		healthOutcome.setValue("0.0");
+		return healthOutcome;
+	}
+
 	private GraphPatientDetailDTO mockGraphPatientDetailsDTO() {
 		GraphPatientDetailDTO graphPatientDetailDTO =
 				new GraphPatientDetailDTO();
@@ -148,5 +146,7 @@ public class HealthOutcomeControllerTest {
 	private PageImpl<GraphPatientDetailDTO> getPageableGraphPatientDetail(PageRequest pageRequest) {
 		return new PageImpl<>(Collections.singletonList(mockGraphPatientDetailsDTO()), pageRequest, 1);
 	}
+
+
 }
 
