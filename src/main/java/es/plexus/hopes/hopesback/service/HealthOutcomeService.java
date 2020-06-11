4205b798ea05521edd09b5d67d1c1ae1f1a7f6ca
angel.broncanorodrig <angel.broncanorodriguez@plexus.es>
2020-06-11 15:13:13 +0200

    #release/v1.0.0
    Se añaden un nuevo parámetro al servicio de HealthOutcomeService para traer los pacientes según resultado PASI

--- src/main/java/es/plexus/hopes/hopesback/service/HealthOutcomeService.java
+++ src/main/java/es/plexus/hopes/hopesback/service/HealthOutcomeService.java
@@ -1,5 +1,15 @@
 package es.plexus.hopes.hopesback.service;
 
+import es.plexus.hopes.hopesback.controller.model.GraphPatientDetailDTO;
+import es.plexus.hopes.hopesback.repository.HealthOutcomeRepository;
+import es.plexus.hopes.hopesback.repository.model.HealthOutcome;
+import es.plexus.hopes.hopesback.repository.model.Patient;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -7,17 +17,6 @@ import java.util.Map;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-
-import es.plexus.hopes.hopesback.controller.model.GraphPatientDetailDTO;
-import es.plexus.hopes.hopesback.repository.HealthOutcomeRepository;
-import es.plexus.hopes.hopesback.repository.model.HealthOutcome;
-import es.plexus.hopes.hopesback.repository.model.Patient;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Service
@@ -34,14 +33,14 @@ public class HealthOutcomeService {
 		return fillResultsByTypes(type);
 	}
 
-	public Page<GraphPatientDetailDTO> getDetailsResultsByType(final String indexType, final Pageable pageable) {
+	public Page<GraphPatientDetailDTO> getDetailsResultsByType(final String indexType, final String result, final Pageable pageable) {
 		log.debug(CALLING_DB);		
-		return  healthOutcomeRepository.getDetailsResultsByType(indexType, pageable);	
+		return  healthOutcomeRepository.getDetailsResultsByType(indexType, result, pageable);
 	}
 	
-	public List<GraphPatientDetailDTO> getDetailsResultsByType(final String indexType) {
+	public List<GraphPatientDetailDTO> getDetailsResultsByType(final String indexType, final String result) {
 		log.debug(CALLING_DB);		
-		return  healthOutcomeRepository.getDetailsResultsByType(indexType);	
+		return  healthOutcomeRepository.getDetailsResultsByType(indexType, result);
 	}
 	
 	public List<Long> getAllPatientsId() {
