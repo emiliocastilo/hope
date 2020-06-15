3a7e0e5a69d29b09d86cb5f4bea8a260c59d663d
Jose Estevez <joseeb@ext.inditex.com>
2020-06-15 09:30:06 +0200

    Fixed issue UTF-8 characters

--- src/main/java/es/plexus/hopes/hopesback/configuration/MessageConfiguration.java
+++ src/main/java/es/plexus/hopes/hopesback/configuration/MessageConfiguration.java
@@ -12,6 +12,7 @@ public class MessageConfiguration {
 	public MessageSource messageSource() {
 	    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
 	    messageSource.setBasename("classpath:/messages/messages");
+	    messageSource.setDefaultEncoding("UTF-8");
 	    return messageSource;
 	}
 }
