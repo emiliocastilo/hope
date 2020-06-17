91942e27b1fb4fa9213379cb47bb8527ea2d60a4
Jose Estevez <joseeb@ext.inditex.com>
2020-06-17 08:53:54 +0200

    Save data score related to index type such as PASI, NAPSI, BSA...

--- src/main/java/es/plexus/hopes/hopesback/service/mapper/HealthOutcomeMapper.java
+++ src/main/java/es/plexus/hopes/hopesback/service/mapper/HealthOutcomeMapper.java
@@ -1,10 +1,28 @@
 package es.plexus.hopes.hopesback.service.mapper;
 
-import es.plexus.hopes.hopesback.controller.model.GraphHealthOutcomeDTO;
-import es.plexus.hopes.hopesback.repository.model.HealthOutcome;
+import java.util.List;
+
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import es.plexus.hopes.hopesback.controller.model.GraphHealthOutcomeDTO;
+import es.plexus.hopes.hopesback.controller.model.HealthOutcomeDTO;
+import es.plexus.hopes.hopesback.repository.model.HealthOutcome;
 
 @Mapper
 public interface HealthOutcomeMapper {
+	
+	HealthOutcomeMapper INSTANCE = Mappers.getMapper(HealthOutcomeMapper.class);
+	
     GraphHealthOutcomeDTO entityToGraphHealthOutcomeDto(HealthOutcome entity);
+    
+    @Mapping(target = "patientId", source = "patient.id")
+    HealthOutcomeDTO entityToHealthOutcomeDto(HealthOutcome entity);
+    
+    List<HealthOutcome> dtoToEntities(List<HealthOutcomeDTO> dto);
+    
+    HealthOutcome dtoToEntity(HealthOutcomeDTO dto);
+    
+    List<HealthOutcomeDTO> entityToHealthOutcomeDtos(List<HealthOutcome> entity);
 }
