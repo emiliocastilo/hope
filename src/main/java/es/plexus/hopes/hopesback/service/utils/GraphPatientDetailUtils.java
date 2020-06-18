71322b2daa26fa8142ffea2e6f77ce9eebc393a4
angel.broncanorodrig <angel.broncanorodriguez@plexus.es>
2020-06-18 11:54:33 +0200

    #release/v1.0.0
    Se modifica paginación en back

--- src/main/java/es/plexus/hopes/hopesback/service/utils/GraphPatientDetailUtils.java
+++ src/main/java/es/plexus/hopes/hopesback/service/utils/GraphPatientDetailUtils.java
@@ -35,13 +35,13 @@ public final class GraphPatientDetailUtils {
         int end = (start + pageable.getPageSize()) > graphPatientDetailList.size() ?
                 graphPatientDetailList.size() : (start + pageable.getPageSize());
 
-       // List<GraphPatientDetailDTO> resultList = graphPatientDetailList.subList(start, end);
+        List<GraphPatientDetailDTO> resultList = graphPatientDetailList.subList(start, end);
 
         if (pageable.getSort().get().findFirst().isPresent()) {
-            orderGraphPatientDetailList(graphPatientDetailList, pageable.getSort());
+            orderGraphPatientDetailList(resultList, pageable.getSort());
         }
 
-        return new PageImpl<>(graphPatientDetailList, pageable, graphPatientDetailList.size());
+        return new PageImpl<>(resultList, pageable, graphPatientDetailList.size());
     }
 
     public static List<GraphPatientDetailDTO> fillGraphPatientDetailDtoList(List<Patient> patients) {
