6f8c0ef05ee5ca3c61ac7e525ebff8e81ca2336c
Maria Vivo <maria.vivoyubero@plexus.es>
2020-08-10 14:54:33 +0200

    [160272]: Modificación del modelo de datos Doctor, User y Roles

--- src/test/java/es/plexus/hopes/hopesback/controller/UserControllerTest.java
+++ src/test/java/es/plexus/hopes/hopesback/controller/UserControllerTest.java
@@ -1,8 +1,11 @@
 package es.plexus.hopes.hopesback.controller;
 
 import es.plexus.hopes.hopesback.controller.model.PasswordDTO;
+import es.plexus.hopes.hopesback.controller.model.PathologyDTO;
 import es.plexus.hopes.hopesback.controller.model.RequestPasswordChangeDTO;
+import es.plexus.hopes.hopesback.controller.model.ServiceDTO;
 import es.plexus.hopes.hopesback.controller.model.UserDTO;
+import es.plexus.hopes.hopesback.controller.model.UserUpdateDTO;
 import es.plexus.hopes.hopesback.repository.model.Token;
 import es.plexus.hopes.hopesback.service.UserService;
 import es.plexus.hopes.hopesback.service.exception.ServiceException;
@@ -81,12 +84,91 @@ public class UserControllerTest {
 		assertNotNull(response);
 	}
 
+	@Test
+	public void findUsersBySearchShouldBeStatusOk() {
+		// given
+		final PageRequest pageRequest = PageRequest.of(1, 5, Sort.by("name"));
+		given(userService.findUsersBySearch(any(String.class), any(Pageable.class)))
+				.willReturn(getPageableUser(pageRequest));
+
+		// when
+		Page<UserDTO> response = userController
+				.findUsersBySearch(mockFullUser().getName(), pageRequest);
+
+		// then
+		assertNotNull(response);
+	}
+
+	@Test
+	public void filterUsersShouldBeStatusOk() {
+		// given
+		final PageRequest pageRequest = PageRequest.of(1, 5, Sort.by("name"));
+		given(userService.filterUsers(any(String.class), any(Pageable.class)))
+				.willReturn(getPageableUser(pageRequest));
+
+		// when
+		Page<UserDTO> response = userController.filterUsers(mockJSONUser(), pageRequest);
+
+		// then
+		assertNotNull(response);
+	}
+
+	@Test
+	public void updateUserShouldBeStatusOk() throws ServiceException {
+		// given
+		given(userService.getOneUserById(anyLong())).willReturn(mockFullUser());
+		given(userService.updateUser(any(UserUpdateDTO.class))).willReturn(mockFullUser());
+
+		// when
+		UserDTO response = userController.updateUser(mockUserUpdateDTO());
+
+		// then
+		assertNotNull(response);
+	}
+
+	@Test(expected = ServiceException.class)
+	public void updateUserShouldBeBadRequestWhenNotFound() throws ServiceException {
+		// when
+		userController.updateUser(mockUserUpdateDTO());
+	}
+
+	@Test
+	public void deleteDoctorShouldBeOk() throws ServiceException {
+		// given
+		given(userService.getOneUserById(anyLong())).willReturn(mockFullUser());
+
+		// when
+		userController.deleteUser(1L);
+
+		// then
+		verify(userService, times(1)).deleteUser(anyLong());
+	}
+
 	private UserDTO mockFullUser() {
 		final UserDTO user = new UserDTO();
 		user.setId(1L);
-		user.setUsername("User Name");
+		user.setUsername("User Username");
 		user.setEmail("User email");
 		user.setRoles(new HashSet<>(Arrays.asList(1L, 2L)));
+		user.setHospitalId(1L);
+		user.setCollegeNumber(1L);
+		user.setDni("User DNI");
+		user.setName("User Name");
+		user.setSurname("User Surname");
+		user.setPhone("User Phone");
+
+		final ServiceDTO serviceDTO = new ServiceDTO();
+		serviceDTO.setId(1L);
+		serviceDTO.setName("Service Name");
+
+		final PathologyDTO pathologyDTO = new PathologyDTO();
+		pathologyDTO.setDescription("Pathology Description");
+		pathologyDTO.setId(1L);
+		pathologyDTO.setName("Pathology Name");
+
+		serviceDTO.setPathologies(new HashSet<>(Collections.singletonList(pathologyDTO)));
+
+		user.setService(serviceDTO);
 
 		return user;
 	}
@@ -147,5 +229,25 @@ public class UserControllerTest {
 		String response = userController.updatePassword(mockPassword());
 
 		assertNotNull(response);
+	}
+
+	private PageImpl<UserDTO> getPageableUser(PageRequest pageRequest) {
+		return new PageImpl<>(Collections.singletonList(mockFullUser()), pageRequest, 1);
+	}
+
+	private String mockJSONUser() {
+		return "{\"name\":\"" + mockFullUser().getName() + "\"}";
+	}
+
+	private UserUpdateDTO mockUserUpdateDTO() {
+		UserUpdateDTO userUpdateDTO = new UserUpdateDTO();
+		userUpdateDTO.setCollegeNumber(2L);
+		userUpdateDTO.setDni("User DNI update");
+		userUpdateDTO.setEmail("User Email update");
+		userUpdateDTO.setId(1L);
+		userUpdateDTO.setName("User Name update");
+		userUpdateDTO.setPhone("User Phone update");
+
+		return userUpdateDTO;
 	}
 }
