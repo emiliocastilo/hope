710727d72df761cc98609f027e3b12ceb727e660
Maria Vivo <maria.vivoyubero@plexus.es>
2020-09-28 12:10:31 +0200

    [160285]: Revisión API medicamentos

--- src/main/java/es/plexus/hopes/hopesback/repository/model/AbstractAudit.java
+++ src/main/java/es/plexus/hopes/hopesback/repository/model/AbstractAudit.java
@@ -1,17 +1,19 @@
 package es.plexus.hopes.hopesback.repository.model;
 
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@Getter
+@Data
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class AbstractAudit {
@@ -19,10 +21,12 @@ public abstract class AbstractAudit {
 	@Column(name = "date_created", updatable = false)
 	@CreatedDate
 	@EqualsAndHashCode.Exclude
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
 	private LocalDateTime dateCreated;
 
 	@Column(name = "date_updated")
 	@LastModifiedDate
 	@EqualsAndHashCode.Exclude
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
 	private LocalDateTime dateUpdated;
 }
