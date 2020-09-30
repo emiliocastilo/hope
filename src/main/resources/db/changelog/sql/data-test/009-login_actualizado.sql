a0be0b6fa0710016c5f590f65ea4ae2bfb9e3e16
Maria Vivo <maria.vivoyubero@plexus.es>
2020-09-30 15:48:37 +0200

    [160272]: Modificación del modelo de datos Doctor, User y Roles

---
+++ src/main/resources/db/changelog/sql/data-test/009-login_actualizado.sql
@@ -0,0 +1,3 @@
+UPDATE hopes.users SET usr_username = 'admin' WHERE usr_name = 'admin';
+UPDATE hopes.users SET usr_username = 'doctor' WHERE usr_name = 'doctor';
+UPDATE hopes.users SET usr_username = 'farmaceutico' WHERE usr_name = 'farmaceutico';
