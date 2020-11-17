94489cd8215c6258dd21ae88d9e43cf835af31fd
Maria Vivo <maria.vivoyubero@plexus.es>
2020-11-17 12:06:39 +0100

    [175420]: Administracion eliminar sección

--- src/main/java/es/plexus/hopes/hopesback/service/exception/ServiceExceptionCatalog.java
+++ src/main/java/es/plexus/hopes/hopesback/service/exception/ServiceExceptionCatalog.java
@@ -16,6 +16,7 @@ import static es.plexus.hopes.hopesback.service.exception.ConstantsServiceCatalo
 import static es.plexus.hopes.hopesback.service.exception.ConstantsServiceCatalog.PHONE_VIOLATION_CONSTRAINT_MESSAGE;
 import static es.plexus.hopes.hopesback.service.exception.ConstantsServiceCatalog.ROLE_CODE_VIOLATION_CONSTRAINT_MESSAGE;
 import static es.plexus.hopes.hopesback.service.exception.ConstantsServiceCatalog.ROLE_NAME_VIOLATION_CONSTRAINT_MESSAGE;
+import static es.plexus.hopes.hopesback.service.exception.ConstantsServiceCatalog.SECTION_ROOT_VIOLATION_CONSTRAINT_MESSAGE;
 
 @Getter
 public enum ServiceExceptionCatalog {
@@ -40,7 +41,8 @@ public enum ServiceExceptionCatalog {
 	NOT_DATE_HISTORIFY_FORM_EXCEPTION(HttpStatus.BAD_REQUEST, "IE-018", NOT_DATE_HISTORIFY_FORM_MESSAGE),
 	NOT_PATIENT_IN_FORM_EXCEPTION(HttpStatus.BAD_REQUEST, "IE-019", NOT_PATIENT_IN_FORM_MESSAGE),
 	NOT_DATE_IN_FORM_EXCEPTION(HttpStatus.BAD_REQUEST, "IE-020", NOT_DATE_IN_FORM_MESSAGE),
-	ROLE_CODE_VIOLATION_CONSTRAINT_EXCEPTION(HttpStatus.BAD_REQUEST, "IE-021", ROLE_CODE_VIOLATION_CONSTRAINT_MESSAGE);
+	ROLE_CODE_VIOLATION_CONSTRAINT_EXCEPTION(HttpStatus.BAD_REQUEST, "IE-021", ROLE_CODE_VIOLATION_CONSTRAINT_MESSAGE),
+	SECTION_ROOT_VIOLATION_CONSTRAINT_EXCEPTION(HttpStatus.BAD_REQUEST, "IE-022", SECTION_ROOT_VIOLATION_CONSTRAINT_MESSAGE);
 
 	private final HttpStatus httpStatus;
 	private final String errorCode;
