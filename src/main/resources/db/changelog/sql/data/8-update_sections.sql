4cea45e9136f68a4c28e85dbd36538182979f3a2
jose.regorodriguez <jose.regorodriguez@plexus.es>
2020-11-25 08:22:48 +0100

    Solucionando problemas de DEV con sql

---
+++ src/main/resources/db/changelog/sql/data/8-update_sections.sql
@@ -0,0 +1,2 @@
+UPDATE hopes.sections SET sec_section_root=1, sec_icon='assets/img/modules/paciente.png' WHERE sec_id= (select sec_id from hopes.sections s where sec_title like 'Paciente');
+UPDATE hopes.sections SET sec_icon='assets/img/modules/gestion-paciente.png' WHERE sec_id= (select sec_id from hopes.sections s where sec_title like 'Gestión de Pacientes');
