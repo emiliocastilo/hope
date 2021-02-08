f9def6c160333c855a1a69445ec743c76b785ae1
gustavo <gustavo.garciadiez@plexus.es>
2021-02-08 09:39:43 +0100

    corregir nombre columnas

--- src/main/resources/db/changelog/sql/data/13-alter_table_medicines_column_name.sql
+++ src/main/resources/db/changelog/sql/data/13-alter_table_medicines_column_name.sql
@@ -1,3 +1,3 @@
--- Cambier nomdres columnas
+-- Corregir nombre columnas tabla nedicines
 ALTER TABLE hopes.medicines RENAME COLUMN med_code_act TO med_code_atc;
 ALTER TABLE hopes.medicines RENAME COLUMN med_code_act_type TO med_code_atc_type;
